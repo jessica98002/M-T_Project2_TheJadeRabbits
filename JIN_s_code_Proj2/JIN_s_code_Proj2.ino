a83e072bd42f275ae08fa16698036f5e0279a967
jessica9802 <jessica892002@gmail.com>
2022-04-07 14:53:38 -0500

    Created variables and initialized servo, button and LED

--- JIN_s_code_Proj2/JIN_s_code_Proj2.ino
+++ JIN_s_code_Proj2/JIN_s_code_Proj2.ino
@@ -1,5 +1,21 @@
+/*
+ AET Chang'e and Hou'Yi Enclosure
+ Team Name: The Jade Rabbits
+ Team Members: Jose Nerio, Anja Danke, Jessica Chen
+ Description: button, servo, and LED program
+ */
+
+//Servo Code library
+ #include<Servo.h>;
+
+//VARIABLES
+Servo myServo;
+const int buttonPin =7;
+
 void setup() {
   // put your setup code here, to run once:
+  myServo.attach(9);
+  pinMode(buttonPin, INPUT);
 
 }
 
